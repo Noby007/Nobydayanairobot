ff905f74b28befcd208c3a4bba802897c615f996
NOBY DAYAN <87512608+Noby007@users.noreply.github.com>
2024-02-11 20:11:56 +0530

    main

--- NobyRobot/__main__.py
+++ NobyRobot/__main__.py
@@ -90,7 +90,7 @@ NEKO_STICKERS = (
     "CAACAgUAAxkBAAIIUWNL5tVBEQAB2gWLSq0ymEPeAj5kmQAC3QEAAuu92VSPj3UkCiNvfioE",
 )
 
-PM_START_TEXT = """
+PM_START_TEXT = 
 ────「 [{}](https://telegra.ph/file/9996ec2eafdef4692102d.jpg) 」────
 *Hola! {},*
 *Meow Meow, Don't Touch Me Without My Permission!*
@@ -99,7 +99,7 @@ PM_START_TEXT = """
 ❍ `{}` *Users, Across* `{}` *Chats.*
 ➖➖➖➖➖➖➖➖➖➖➖➖➖
 ➛ Try The Guidelines Button Below To Know My Abilities ××
-"""
+
 
 buttons = [
     [
